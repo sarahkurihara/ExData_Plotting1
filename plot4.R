37a8b1f55e812bd139f6bcbc725ea9ff83c31f4e
skuri <61945903+sarahkurihara@users.noreply.github.com>
2020-05-09 17:44:56 -0400

    indent for readability

--- plot4.R
+++ plot4.R
@@ -1,18 +1,18 @@
-##change number of plots allowed
-par(mfrow = c(2,2))
-par(mar = c(5, 5, 5, 5))
+ ##change number of plots allowed
+  par(mfrow = c(2,2))
+  par(mar = c(5, 5, 5, 5))
 
-##plot 1 in 1,1
-plot2fn()
+ ##plot 1 in 1st position
+  plot2fn()
 
-##new plot in 1,2
-voltage <- as.numeric(as.character(data$Voltage))
-plot(datetime, voltage, type = "l")
+ ##new plot in 2nd position
+  voltage <- as.numeric(as.character(data$Voltage))
+  plot(datetime, voltage, type = "l")
 
-##plot 3 in 3rd position
-plot3fn()
+ ##plot 3 in 3rd position
+  plot3fn()
 
-##new plot in 4th position
-Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
-plot(datetime, Global_reactive_power, type = "l")
-axis(side = 2, at = c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5))
+ ##new plot in 4th position
+  Global_reactive_power <- as.numeric(as.character(data$Global_reactive_power))
+  plot(datetime, Global_reactive_power, type = "l")
+  axis(side = 2, at = c(0.0, 0.1, 0.2, 0.3, 0.4, 0.5))
