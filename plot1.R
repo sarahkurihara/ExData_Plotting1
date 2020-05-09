38cab586eef7cdd36f57299d3df7cc3fc256a494
skuri <61945903+sarahkurihara@users.noreply.github.com>
2020-05-09 17:43:48 -0400

    indent for readability

--- plot1.R
+++ plot1.R
@@ -1,21 +1,20 @@
 plot1fn <- function() {
-##load data
-data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
+     ##load data
+     data <- read.csv("household_power_consumption.txt", header = TRUE, sep = ";")
 
-##subset data from the dates 2007-02-01 and 2007-02-02
-data <- subset(data, Date == "2/2/2007" | Date == "1/2/2007")
+     ##subset data from the dates 2007-02-01 and 2007-02-02
+     data <- subset(data, Date == "2/2/2007" | Date == "1/2/2007")
 
-##convert data to character and then numeric
-GAP <- as.numeric(as.character(data$Global_active_power))
+     ##convert data to character and then numeric
+     GAP <- as.numeric(as.character(data$Global_active_power))
 
-##Initialize histogram
-hist(GAP, 
-     col = "red", 
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)"
-)
+     ##Initialize histogram
+     hist(GAP, 
+          col = "red", 
+          main = "Global Active Power",
+          xlab = "Global Active Power (kilowatts)"
+     )
 
-##Change axes
-axis(side = 2, at = c(200,400,600,800,1000,1200))
-
+     ##Change axes
+     axis(side = 2, at = c(200,400,600,800,1000,1200))
 }
